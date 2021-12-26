64d766ff7b7eb4466162bf3c7c921a518f21791f
Rob Formicola <robertformicola@gmail.com>
2021-12-26 14:17:56 -0800

    first commit

---
+++ java.js
@@ -0,0 +1,17 @@
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+		document.getElementById("navbar").style.padding = "5px 10px";
+		document.getElementById("navbar").style.background = "#f1f1f1";
+		document.getElementById("logo").style.width = "50px";
+		document.getElementById("logo").style.height = "50px";
+	} else {
+		document.getElementById("navbar").style.padding = "10px 10px";
+		document.getElementById("navbar").style.background = "transparent";
+		document.getElementById("logo").style.width = "120px";
+		document.getElementById("logo").style.height = "120px";
+	}
+}
