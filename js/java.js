6ca270c47a65770db7ff12da8b343e7ea4da4af9
Rob Formicola <robertformicola@gmail.com>
2021-12-27 03:00:49 -0800

    HomepageUpdate

--- js/java.js
+++ js/java.js
@@ -5,7 +5,7 @@ window.onscroll = function () {
 function scrollFunction() {
 	if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
 		document.getElementById("navbar").style.padding = "5px 10px";
-		document.getElementById("navbar").style.background = "#f1f1f1";
+		document.getElementById("navbar").style.background = "#000000";
 		document.getElementById("logo").style.width = "50px";
 		document.getElementById("logo").style.height = "50px";
 	} else {
